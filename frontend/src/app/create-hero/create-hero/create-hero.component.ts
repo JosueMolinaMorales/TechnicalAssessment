63308dd01b54cae4446d825be844932fd0a1ab1b
Josue Molina Morales <molinajosue92@hotmail.com>
2021-11-13 16:06:49 -0500

    Updates to Task 2

--- frontend/src/app/create-hero/create-hero/create-hero.component.ts
+++ frontend/src/app/create-hero/create-hero/create-hero.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Hero } from 'src/app/types/Hero';
 import { BackendService } from '../../services/backend.service';
 
 @Component({
@@ -8,12 +10,39 @@ import { BackendService } from '../../services/backend.service';
 })
 export class CreateHeroComponent implements OnInit {
 
-  constructor(private backend: BackendService) { }
+  newHero: Hero = {name:"", level:-1, class:"", id:""};
+  heroCreated: boolean = false;
+  constructor(private backend: BackendService, private router: Router) { }
 
   ngOnInit(): void {
   }
+  
+  isHeroReady(){
+    if(this.newHero.name.length != 0 && this.newHero.level > -1 && this.newHero.class.length != 0){
+      this.createHero();
+    }
+  } 
 
-  async createHero(heroname:string, heroclass:string, herolevel:number){
-    await this.backend.createAHero({name:heroname, level:herolevel, class:heroclass, id:""}); 
+  addName(name:string): void{
+    this.newHero.name = name;
+    this.isHeroReady()
+  }
+
+  addClass(Hclass:string): void{
+    this.newHero.class = Hclass;
+    this.isHeroReady()
+  }
+
+  addLevel(HLevel: number): void{
+    this.newHero.level = HLevel;
+    this.isHeroReady()
+  }
+
+  async createHero(){
+    await this.backend.createAHero(this.newHero); 
+  }
+
+  goBackToHomePage(){
+    this.router.navigate(['/']);
   }
 }

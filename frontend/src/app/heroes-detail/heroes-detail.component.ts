dfa878d84a4f59ea643213b2e617fdfe30ea0c7d
Josue Morales <molinajosue92@hotmail.com>
2021-11-15 19:51:15 -0500

    Updates To Task 2

--- frontend/src/app/heroes-detail/heroes-detail.component.ts
+++ frontend/src/app/heroes-detail/heroes-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { Hero } from "../types/Hero";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { BackendService } from "../services/backend.service";
 
 @Component({
@@ -12,13 +12,15 @@ export class HeroDetailComponent implements OnInit{
     
     @Input() hero: Hero = {name: "", id: "123", level: 0, class: ""};
     id: string | null = "";
-    constructor(private backend: BackendService, private route: ActivatedRoute) { }
+    constructor(private backend: BackendService, private route: ActivatedRoute, private router: Router) { }
     
     
     async ngOnInit(): Promise<void>{
         //this.test = await this.backend.getAHero(this.heroes[0]);
         this.id = this.route.snapshot.paramMap.get("id");
-        if(this.id)
-            this.hero = await this.backend.getAHero(this.id)
+        
+        if(this.id){
+            this.hero = await this.backend.getAHero(this.id);
+        }
     }
 }
--- frontend/src/app/services/backend.service.ts
+++ frontend/src/app/services/backend.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Hero } from '../types/Hero';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   /**
    * Gets a list of heroes from the backend API
@@ -22,10 +23,14 @@ export class BackendService {
   /**
    * Gets a details of a hero
    * @param heroId a string that contains the id of the hero that you are trying to find
-   * @returns String containing hero details
+   * @returns a Hero
    */
   getAHero(heroId: string | null): Promise<Hero>{
-    return this.http.get<Hero>(`${environment.api}/heroes/${heroId}`).toPromise();
+    return this.http.get<Hero>(`${environment.api}/heroes/${heroId}`).toPromise().catch<Hero>(err => {
+      window.alert("ERROR, No hero found with ID: " + heroId);
+      this.router.navigate(['/']);
+      return {name: "", id: "", level: 0, class:""};
+    });
   }
 
   /**
